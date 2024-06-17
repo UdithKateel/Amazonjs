ff55c1e475ea4d70bfd2831a7c049f3ec43e4e83
Udith Kateel <udithshetty555@gmail.com>
2024-06-17 17:50:24 +0530

    v7

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,1 +1,24 @@
-const cart =[]
+ const cart =[]
+ function addtocart(productId) {
+  let matchingproduct;
+
+  cart.forEach((item) => {
+    if (productId === item.productId) {
+      matchingproduct = item;
+    }
+  });
+  if (matchingproduct) {
+    matchingproduct.Quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      Quantity: 1,
+    });
+  }
+}
+let cartcontents = 0;
+function updatecartquantity() {
+  cart.forEach((item) => {
+    cartcontents += item.Quantity;
+  });
+}
--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,87 +1,69 @@
-let productsHTML=''
-products.forEach((product)=>{
-                productsHTML+= ` 
-                    <div class="product-container">
-                    <div class="product-image-container">
-                        <img class="product-image"
-                        src=${product.image}>
-                    </div>
+let productsHTML = "";
+products.forEach((product) => {
+  productsHTML += ` 
+                            <div class="product-container">
+                            <div class="product-image-container">
+                                <img class="product-image"
+                                src=${product.image}>
+                            </div>
 
-                    <div class="product-name limit-text-to-2-lines">
-                        ${product.name}
-                    </div>
+                            <div class="product-name limit-text-to-2-lines">
+                                ${product.name}
+                            </div>
 
-                    <div class="product-rating-container">
-                        <img class="product-rating-stars"
-                        src="images/ratings/rating-${product.rating.stars*10}.png">
-                        <div class="product-rating-count link-primary">
-                        ${product.rating.count}
-                        </div>
-                    </div>
+                            <div class="product-rating-container">
+                                <img class="product-rating-stars"
+                                src="images/ratings/rating-${
+                                  product.rating.stars * 10
+                                }.png">
+                                <div class="product-rating-count link-primary">
+                                ${product.rating.count}
+                                </div>
+                            </div>
 
-                    <div class="product-price">
-                        $${(product.priceCents/100).toFixed(2)}
-                    </div>
+                            <div class="product-price">
+                                $${(product.priceCents / 100).toFixed(2)}
+                            </div>
 
-                    <div class="product-quantity-container">
-                        <select>
-                        <option selected value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        </select>
-                    </div>
+                            <div class="product-quantity-container">
+                                <select>
+                                <option selected value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                                </select>
+                            </div>
 
-                    <div class="product-spacer"></div>
+                            <div class="product-spacer"></div>
 
-                    <div class="added-to-cart">
-                        <img src="images/icons/checkmark.png">
-                        Added
-                    </div>
+                            <div class="added-to-cart">
+                                <img src="images/icons/checkmark.png">
+                                Added
+                            </div>
 
-                    <button class="add-to-cart-button button-primary js-add-to-cart"
-                    data-product-id="${product.id}">
-                        Add to Cart
-                    </button>
-                    
-                    </div>`
-                    
+                            <button class="add-to-cart-button button-primary js-add-to-cart"
+                            data-product-id="${product.id}">
+                                Add to Cart
+                            </button>
+                            
+                            </div>`;
+});
 
-},
-)
-document.querySelector('.products-grid').innerHTML=productsHTML
-document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
-  button.addEventListener('click',()=>{
-   const productId=button.dataset.productId
-let matchingproduct;
+document.querySelector(".products-grid").innerHTML = productsHTML;
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+    addtocart(productId);
+    updatecartquantity();
 
-   cart.forEach((item)=>{
-    if(productId===item.productId){
-      matchingproduct=item
-    }
-   })
-   if(matchingproduct){
-    matchingproduct.Quantity+=1
-   }
-   else{
-     cart.push({
-    productId:productId,
-    Quantity:1
-   })
-  } 
-   console.log(cart)
-   let cartcontents=0
-   cart.forEach((item)=>{
-    cartcontents+=item.Quantity
-   })
-   document.querySelector('.js-cart-quantity').innerText=cartcontents
-  })
+    console.log(cart);
 
- 
-})
+    document.querySelector(".js-cart-quantity").innerText = cartcontents;
+  });
+});
