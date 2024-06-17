9ffb1d9a346fd187a5d406ff446c08c21d431a54
Udith Kateel <udithshetty555@gmail.com>
2024-06-17 21:35:17 +0530

    cart display complete

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,6 @@
- export let cart =[
+ export let cart =JSON.parse(localStorage.getItem('cart'))
+ if(!cart){
+    cart=[
     {
         productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
         Quantity: 2
@@ -8,6 +10,11 @@
         Quantity:3
     }
  ]
+ }
+ 
+ function savetostorage(){
+    localStorage.setItem('cart',JSON.stringify(cart))
+}
  export function addtocart(productId) {
   let matchingproduct;
 
@@ -24,13 +31,16 @@
       Quantity: 1,
     });
   }
+  savetostorage()
 }
 
 export function updatecartquantity() {
-    let cartcontents = 0;
+  let cartcontents = 0;
   cart.forEach((item) => {
     cartcontents += item.Quantity;
   });
+  savetostorage()
+  return cartcontents; // Return the total cart contents
 }
 export function removeFromCart(productID){
     let newCart=[]
@@ -40,4 +50,6 @@ export function removeFromCart(productID){
         }
     })
     cart=newCart
+    savetostorage()
 }
+
